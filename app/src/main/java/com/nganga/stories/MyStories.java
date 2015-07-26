d6e4ca3b5f09989837eda1f0baab58ab80d053a8
mtnjoroge <mtnjoroge@gmail.com>
2015-07-26 23:10:09 +0300

    class constructor for the stories

--- app/src/main/java/com/nganga/stories/MyStories.java
+++ app/src/main/java/com/nganga/stories/MyStories.java
@@ -4,4 +4,17 @@ package com.nganga.stories;
  * Created by nganga on 7/26/15.
  */
 public class MyStories {
+
+    String title;
+    String desc;
+    int image;
+
+    public MyStories(String title, String desc, int image)
+    {
+        this.title = title;
+        this.desc = desc;
+        this.image = image;
+    }
+
+
 }
