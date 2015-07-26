02963c042e473705ba29a2b57d354f6190eef933
mtnjoroge <mtnjoroge@gmail.com>
2015-07-26 22:35:47 +0300

    created Adapter#RecyclerView#Layoutmanager objects

--- app/src/main/java/com/nganga/stories/MainActivity.java
+++ app/src/main/java/com/nganga/stories/MainActivity.java
@@ -1,12 +1,20 @@
 package com.nganga.stories;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mlayoutmanager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
