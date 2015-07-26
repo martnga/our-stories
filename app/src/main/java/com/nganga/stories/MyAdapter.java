dd921d4cfc3ee8c6da1df2c10318211296f7c16e
mtnjoroge <mtnjoroge@gmail.com>
2015-07-26 23:56:47 +0300

    view objects for the view components

--- app/src/main/java/com/nganga/stories/MyAdapter.java
+++ app/src/main/java/com/nganga/stories/MyAdapter.java
@@ -1,9 +1,11 @@
 package com.nganga.stories;
 
+import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,10 +21,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
-        public ViewHolder(TextView v) {
+        public View view;
+        public ViewHolder(View v) {
             super(v);
-            mTextView = v;
+            view = v;
         }
     }
 
@@ -49,14 +51,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public void onBindViewHolder(ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mTextView.setText(mDataset[position]);
+        TextView  title = (TextView) holder.view.findViewById(R.id.title);
+        TextView  desc = (TextView) holder.view.findViewById(R.id.desc);
+        ImageView  imageView = (ImageView) holder.view.findViewById(R.id.imageView);
 
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return myStories.size();
     }
 }
 
