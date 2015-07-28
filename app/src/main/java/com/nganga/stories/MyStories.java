2ef70b4824c5f374fdf8f52daaf869937079c335
mtnjoroge <mtnjoroge@gmail.com>
2015-07-28 08:29:47 +0300

    lined myStories class to the fragment XML

--- app/src/main/java/com/nganga/stories/MyStories.java
+++ app/src/main/java/com/nganga/stories/MyStories.java
@@ -1,32 +1,41 @@
 package com.nganga.stories;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by nganga on 7/26/15.
  */
-public class MyStories extends Fragment{
+public class MyStories extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+    }
 
     String title;
     String desc;
     //int image;
 
-     //class constructor for the story components
-    public MyStories(String title, String desc/*, int image*/)
-    {
+
+    //class constructor for the story components
+    public MyStories(String title, String desc/*, int image*/) {
         this.title = title;
         this.desc = desc;
         //this.image = image;
     }
 
     //getter methods
-   public String getTitle()
-   {
-       return title;
-   }
+    public String getTitle() {
+        return title;
+    }
 
-    public String getDesc()
-    {
+    public String getDesc() {
         return desc;
     }
 
